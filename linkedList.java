95d72ccfc47f808c1ac8407064b42007e4fa1736
Spikree <avimahalingpure10@gmail.com>
2024-01-25 21:14:33 +0530

    Linked list

---
+++ linkedList.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class linkedList {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        // linkedList.push("A");
+        // linkedList.push("B");
+        // linkedList.push("C");
+        // linkedList.push("D");
+        // linkedList.push("F");
+        // linkedList.pop();
+
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        // linkedList.poll();
+        // linkedList.pop();
+
+        linkedList.add(4, "E");
+        // linkedList.remove("E");
+
+        System.out.println(linkedList.indexOf("F"));
+
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+        linkedList.addFirst("0");
+        linkedList.addLast("G");
+
+        System.out.println(linkedList);
+    }
+}
